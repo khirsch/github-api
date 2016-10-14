21711bbb19f82cdf45cc41ef5dbc5c3b3a689024
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-10-14 11:03:23 -0700

    add functionality to display user's name and profile url

--- js/githunt.js
+++ js/githunt.js
@@ -1,13 +1,16 @@
 var apiKey = require('./../.env').apiKey;
 
-function User() {
+function User(username) {
+  this.username = username;
 }
 
 User.prototype.getInfo = function(username) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#name').text(response.name);
+    $('#html_url').text(response.html_url);
   }).fail(function(error) {
-    console.log(error.responseJSON.message);
+    $('#output').text(error.responseJSON.message);
   });
 };
 
